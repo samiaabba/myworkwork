8bad7682ef67f8669b8b70a93e4a2fce5ed87b1e
samia <melinasamia1984@gmail.com>
2026-09-17 13:58:13 -0400

    update the second time

--- script1.sh
+++ script1.sh
@@ -1,3 +1,4 @@
 echo " hi this is my homework practice1"
 echo " hi this is my homework practice2234"
+echo " hi this is my homework5642t practice2234"
 
--- script2.sh
+++ script2.sh
@@ -1,2 +1,3 @@
 echo " hi this is my homework practice2"
 echo " hi this is my homework practicedfdgfgse1"
+echo " hi this is my ggththrthomework practice2234"
