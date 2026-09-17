20d447ff30aba96a5b4c1bfa43387ebe3f8c92e5
samia <melinasamia1984@gmail.com>
2026-09-17 13:52:51 -0400

    update document

--- script1.sh
+++ script1.sh
@@ -1,2 +1,3 @@
 echo " hi this is my homework practice1"
+echo " hi this is my homework practice2234"
 
--- script2.sh
+++ script2.sh
@@ -1,1 +1,2 @@
 echo " hi this is my homework practice2"
+echo " hi this is my homework practicedfdgfgse1"
